e252f380107cfb366a540629bc9c3f2e9d705190
Халтаев Никита Сергеевич <21073@iat.iat>
2024-10-02 14:45:38 +0800

    themes page create | themes route create

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\{ ProfileController, ThemeController, CategoryController };
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,5 +23,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/theme', function () {
+    return Inertia::render('ThemePage', [
+        'canLogin' => Route::has('theme')
+    ]);
+})->name('theme');
 
 require __DIR__.'/auth.php';
