ebc051731f9c34e4d3f7921bc0c6f752259f93d7
Халтаев Никита Сергеевич <21073@iat.iat>
2024-10-12 16:24:32 +0800

    ашч

--- resources/js/Components/MainCategoriesItem.vue
+++ resources/js/Components/MainCategoriesItem.vue
@@ -38,7 +38,9 @@ import { ref } from 'vue';
     <ul class="main__categories-list flex flex-col gap-2 text-gray-800 dark:text-white mb-5">
         <li v-for="category in $page.props.categories" :key="category.id" class="mb-4 relative">
             <a href="#!" class="transition-all hover:text-red-400 font-medium p-2 bg-gray-100 rounded-md hover:border-l-2 hover:border-red-400">{{ category.name }}</a>
-            <button @click="deleteCategory(category.id)" class="absolute right-0">X</button>
+            <button v-if="$page.props.auth.user.is_admin == 1" @click="deleteCategory(category.id)" class="absolute right-0">
+                <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM8.96963 8.96965C9.26252 8.67676 9.73739 8.67676 10.0303 8.96965L12 10.9393L13.9696 8.96967C14.2625 8.67678 14.7374 8.67678 15.0303 8.96967C15.3232 9.26256 15.3232 9.73744 15.0303 10.0303L13.0606 12L15.0303 13.9696C15.3232 14.2625 15.3232 14.7374 15.0303 15.0303C14.7374 15.3232 14.2625 15.3232 13.9696 15.0303L12 13.0607L10.0303 15.0303C9.73742 15.3232 9.26254 15.3232 8.96965 15.0303C8.67676 14.7374 8.67676 14.2625 8.96965 13.9697L10.9393 12L8.96963 10.0303C8.67673 9.73742 8.67673 9.26254 8.96963 8.96965Z" fill="#f87171"></path> </g></svg>
+            </button>
         </li>
         <li v-if="$page.props.auth.user && $page.props.auth.user.is_admin == 1">
             <form @submit.prevent="createCategory" method="POST" class="flex items-center gap-2">
