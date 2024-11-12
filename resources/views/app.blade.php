9b94f41cf327642e85895d0725ae11250f711ac7
Халтаев Никита Сергеевич <21073@iat.iat>
2024-11-12 10:50:31 +0800

    first diagram create | no data import

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,4 +35,5 @@
         </button>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 </html>
