794935448738104bc3ab2d3fbcc4710a6c8b75c8
painkhn <1fotice@gmail.com>
2024-12-02 12:43:56 +0800

    delete theme fix

---
+++ resources/js/Components/DeleteTheme.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { defineProps, defineEmits } from 'vue';
+
+    const props = defineProps({
+        theme: {
+            type: Object,
+            required: true,
+        },
+    });
+
+    const emit = defineEmits(['delete', 'cancel']);
+
+    const handleDelete = () => {
+        emit('delete', props.theme.id);
+    };
+
+    const handleCancel = () => {
+        emit('cancel');
+    };
+</script>
+
+<template>
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 z-10 bg-zinc-600 border border-zinc-900 shadow-md rounded-xl p-5 !max-w-xl w-full h-auto">
+        <p class="text-white mb-5">Вы уверены, что хотите удалить тему "{{ $page.props.theme.name }}"?</p>
+        
+        <div class="flex items-center gap-5">
+            <button v-if="$page.props.auth.user.is_admin == 1" @click="handleDelete" class="px-4 py-2 bg-zinc-700 rounded-md transition-all hover:bg-zinc-800">
+                Уверен
+            </button>
+            <button @click="handleCancel" class="px-4 py-2 bg-zinc-700 rounded-md transition-all hover:bg-zinc-800">Отмена</button>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/ThemePage.vue
+++ resources/js/Pages/ThemePage.vue
@@ -6,6 +6,7 @@
     import CommentForm from '@/Components/CommentForm.vue';
     import { Head, Link, usePage } from '@inertiajs/vue3';
     import ComplaintForm from '@/Components/ComplaintForm.vue';
+    import DeleteTheme from '@/Components/DeleteTheme.vue';
 
     const goBack = () => {
         window.history.back();
@@ -139,6 +140,23 @@
         }
     }
 
+    const isDeleteVisible = ref(false)
+    const selectedTheme = ref(null)
+
+    const deleteVisibleChange = (theme) => {
+        selectedTheme.value = theme
+        isDeleteVisible.value = !isDeleteVisible.value
+    }
+
+    const handleDelete = (themeId) => {
+        deleteTheme(themeId)
+        isDeleteVisible.value = false
+    }
+
+    const handleCancel = () => {
+        isDeleteVisible.value = false
+    }
+
     onMounted(() => {
         console.log('Рез:', props.theme.id);
 
@@ -230,9 +248,15 @@
                     </form>
                 </li>
                 <li>
-                    <button v-if="$page.props.auth.user.is_admin == 1 ||$page.props.auth.user.id == $page.props.theme.user.id " @click="deleteTheme($page.props.theme.id)" class="px-5 py-2 bg-red-400 rounded-md text-white font-semibold transition-all hover:bg-red-300">
+                    <button v-if="$page.props.auth.user.is_admin == 1 ||$page.props.auth.user.id == $page.props.theme.user.id " @click="deleteVisibleChange(theme)" class="px-5 py-2 bg-red-400 rounded-md text-white font-semibold transition-all hover:bg-red-300">
                         Удалить тему
                     </button>
+                    <DeleteTheme
+                        :theme="selectedTheme" 
+                        v-if="isDeleteVisible" 
+                        @delete="handleDelete" 
+                        @cancel="handleCancel" 
+                    />
                 </li>
                 <li>
                     <button @click="downloadTheme($page.props.theme.id)" class="px-5 py-2 bg-red-400 rounded-md text-white font-semibold transition-all hover:bg-red-300">
